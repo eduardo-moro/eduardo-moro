026de7d9f23f1edce2d15fa2dea21a661113f966
eduardo-moro <dev.eduardomoro@gmail.com>
2025-09-16 16:33:16 -0300

    [fix]: images on markdown

--- src/components/personal/markdown-image.tsx
+++ src/components/personal/markdown-image.tsx
@@ -1,21 +1,37 @@
 "use client"
 
-import React from 'react';
-import Image from 'next/image';
+import React, { useState, useEffect, useRef } from 'react';
 
 interface MarkdownImageProps extends React.ImgHTMLAttributes<HTMLImageElement> {
   handleImageClick: (src: string, alt: string) => void;
 }
 
 export default function MarkdownImage({ handleImageClick, ...props }: MarkdownImageProps) {
+  const [isTallImage, setIsTallImage] = useState(false);
+  const imgRef = useRef<HTMLImageElement>(null);
+
+  useEffect(() => {
+    if (imgRef.current) {
+      const img = imgRef.current;
+      const checkImageDimensions = () => {
+        if (img.naturalHeight > img.naturalWidth) {
+          setIsTallImage(true);
+        }
+      };
+      if (img.complete) {
+        checkImageDimensions();
+      } else {
+        img.onload = checkImageDimensions;
+      }
+    }
+  }, [props.src]);
+
   return (
-    <Image
+    <img
+      ref={imgRef}
       alt={props.alt || ""}
       {...props}
-      width={500}
-      height={500}
-      src={"/"}
-      className={`${props.className || ""}`}
+      className={`${props.className || ""} ${isTallImage ? "tall-image" : ""}`}
       onClick={() => handleImageClick(props.src as string || "", props.alt || "")}
     />
   );
